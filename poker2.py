452656e2861c704c81e6d7ae8be6d684f185b580
Yunyoung Ma <yunyoung@ynbook.local>
2023-09-13 08:28:19 +0900

    update on class card and new functions for deck

--- poker2.py
+++ poker2.py
@@ -1,42 +1,58 @@
+import random
+
 class Card(object):
     def __init__(self, suit, number):
         self.suit = suit
         self.number = number
 
+    def __repr__(self):
+        number_name = ""
+        suit_name = ""
+
+        if self.suit == 0:
+            suit_name = "Spades"
+        elif self.suit == 1:
+            suit_name = "Hearts"
+        elif self.suit == 2:
+            suit_name = "Diamonds"
+        #elif self.suit == 3:
+            #suit_name = "Clubs"
+        else:
+            suit_name = "Clubs"
+        
+        if self.number == 12:
+            number_name = "Ace"
+        elif self.number == 11:
+            number_name = "King"
+        elif self.number == 10:
+            number_name = "Queen"
+        elif self.number == 9:
+            number_name = "Jack"
+        else:
+            number_name = str(self.number+2)
+
+        return number_name + " of " + suit_name
+
 class Deck(list):
     def __init__(self):
         suits = list(range(4))
         numbers = list(range(13))
-        for i in suits:
-            for j in numbers:
-                self.append(Card(i, j))
-    
-    def printDeck(self):
-        for i in self:
-            if i.suit == 0:
-                s = "Spades"
-            elif i.suit == 1:
-                s = "Hearts"
-            elif i.suit == 2:
-                s = "Diamonds"
-            else:
-                s = "Clubs"
+        for i in numbers:
+            for j in suits:
+                self.append(Card(j, i))
 
-            if i.number == 12:
-                n = "A"
-            elif i.number == 11:
-                n = "K"
-            elif i.number == 10:
-                n = "Q"
-            elif i.number == 9:
-                n = "J"
-            else:
-                n = str(i.number + 2)
+    def shuffle(self):
+        random.shuffle(self)
 
-            print(n + " of " + s)
+    def deal(self, player):
+        for i in range(0, 5):
+            player.append(self.pop(0))
+
+    def showdeck(self):
+        for card in self:
+            print(card)
             
-        
 
 deck1 = Deck()
-deck1.printDeck()
+deck1.showdeck()
 
